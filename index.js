1b7a4053310dee219733f360760dae97b9d6db7f
lim-jiahao <lim.jiahaoo@gmail.com>
2021-10-31 19:42:40 +0800

    comfortable: categories with links to recipes in category page

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const getAllRecipes = (req, res) => {
       return;
     }
 
-    res.status(200).render('index', data);
+    const categories = [...new Set(data.recipes.map((recipe) => recipe.category ?? 'Uncategorised'))];
+    console.log(categories);
+
+    res.status(200).render('index', { recipes: data.recipes, categories });
   });
 };
 
@@ -33,6 +36,28 @@ const getRecipeByIndex = (req, res) => {
     }
 
     res.status(200).render('recipe', recipe);
+  });
+};
+
+const getRecipesByCategory = (req, res) => {
+  read(FILENAME, (err, data) => {
+    if (err) {
+      console.error('Read error', err);
+      res.status(500).send(err);
+      return;
+    }
+
+    const { category } = req.params;
+    let recipes = data.recipes.map((recipe, index) => ({ ...recipe, index }));
+    if (category === 'uncategorised') recipes = recipes.filter((el) => !el.category);
+    else recipes = recipes.filter((el) => el.category?.toLowerCase() === category);
+
+    if (recipes.length === 0) {
+      res.status(404).send('Sorry, we cannot find that!');
+      return;
+    }
+
+    res.status(200).render('category', { recipes });
   });
 };
 
@@ -107,6 +132,7 @@ const getRecipeByIndex = (req, res) => {
 
 app.get('/', getAllRecipes);
 app.get('/recipe/:index', getRecipeByIndex);
+app.get('/category/:category', getRecipesByCategory);
 // app.get('/yield/:num', getRecipesByYield);
 // app.get('/recipe-label/:label', getRecipesByLabel);
 
